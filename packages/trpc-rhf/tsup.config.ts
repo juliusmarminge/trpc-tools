0618c494c56b9b483e6c61e9cb167f20492430e9
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-09-23 15:20:37 +0200

    bit more setup

---
+++ packages/trpc-rhf/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  minify: true,
+  sourcemap: true,
+  target: "esnext",
+  outDir: "dist",
+});
