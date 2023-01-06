eb0ee8a9e7b86163116ac631d8b18ff357960fe9
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-06 01:40:12 +0100

    proxy

--- packages/form/src/proxy.ts
+++ packages/form/src/proxy.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 type ProxyCallback = (path: string[], args: unknown[]) => unknown;
 
 function createInnerProxy(callback: ProxyCallback, path: string[]) {
@@ -7,6 +8,7 @@ function createInnerProxy(callback: ProxyCallback, path: string[]) {
       return createInnerProxy(callback, [...path, key]);
     },
     apply(_target, _thisArg, args) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       return callback(args, path);
     },
   });
@@ -14,9 +16,21 @@ function createInnerProxy(callback: ProxyCallback, path: string[]) {
   return proxy;
 }
 
-/**
- * Creates a proxy that calls the callback with the path and arguments
- */
 export function createRecursiveProxy(callback: ProxyCallback) {
   return createInnerProxy(callback, []);
 }
+
+export const createFlatProxy = <TFaux>(
+  callback: (path: keyof TFaux & string) => unknown,
+): TFaux => {
+  return new Proxy(() => {}, {
+    get(_obj, name) {
+      if (typeof name !== "string" || name === "then") {
+        // special case for if the proxy is accidentally treated
+        // like a PromiseLike (like in `Promise.resolve(proxy)`)
+        return undefined;
+      }
+      return callback(name as keyof TFaux & string);
+    },
+  }) as TFaux;
+};
